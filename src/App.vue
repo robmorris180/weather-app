8c1987fb725e0c0ac099452afe7eea213a41d922
Rob <rob.morris@gocompare.com>
2020-04-19 20:54:12 +0100

    Add search-weather component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">Emoji Weather App ⛅</a>
+      <a class="navbar-brand" href="#">Weather App ⛅</a>
     </nav>
     <main>
-      <div class="input-group mb-3">
-        <input
-          v-model="location"
-          @keyup.enter="updateLocation"
-          type="text"
-          class="form-control"
-          placeholder="Enter location"
-        />
-        <div class="input-group-append">
-          <button
-            @click="updateLocation"
-            class="btn btn-outline-secondary"
-            type="button"
-          >
-            Go
-          </button>
-        </div>
-      </div>
+      <search-weather @update:location="updateLocation" />
       <latest-weather v-if="forecast" :forecast="forecast" :address="address" />
     </main>
   </div>
@@ -30,16 +13,17 @@
 <script>
 import API from "@/lib/API";
 import LatestWeather from "./components/LatestWeather.vue";
+import SearchWeather from "./components/SearchWeather.vue";
 
 export default {
   name: "App",
   components: {
-    LatestWeather
+    LatestWeather,
+    SearchWeather
   },
   data() {
     return {
       address: "",
-      location: "",
       forecast: null
     };
   },
@@ -52,8 +36,8 @@ export default {
       this.address = address.name;
       this.forecast = forecast;
     },
-    updateLocation() {
-      API.getCoordinates(this.location).then(result => {
+    updateLocation(location) {
+      API.getCoordinates(location).then(result => {
         this.loadWeather(result.coord.lat, result.coord.lon);
       });
     }
---
+++ src/components/SearchWeather.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="updateLocation">
+    <div class="input-group mb3">
+      <input
+        v-model="location"
+        type="text"
+        class="form-control"
+        placeholder="Enter location"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="submit">
+          Go
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchWeather",
+  data() {
+    return {
+      location: ""
+    };
+  },
+  methods: {
+    updateLocation() {
+      this.$emit("update:location", this.location);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>

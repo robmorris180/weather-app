11e6039bd44c118c574cbbe2f1d85d0db2d47e24
Rob <rob.morris@gocompare.com>
2020-04-19 21:29:24 +0100

    Tidy up latest-weather component

--- src/components/LatestWeather.vue
+++ src/components/LatestWeather.vue
@@ -3,10 +3,7 @@
     <div class="card-header">Latest weather in: {{ address }}</div>
     <div class="card-body">
       <h5 class="card-title">{{ forecast.current.weather[0].main }}</h5>
-      <p class="card-text">
-        {{ icons[forecast.current.weather[0].icon] }}
-        {{ forecast.current.temp }} °C
-      </p>
+      <p class="card-text">Temperature: {{ forecast.current.temp }} °C</p>
     </div>
   </div>
 </template>
@@ -17,21 +14,6 @@ export default {
   props: {
     forecast: Object,
     address: String
-  },
-  data() {
-    return {
-      icons: {
-        "01d": "🌞",
-        "02d": "⛅",
-        "03d": "🌤",
-        "04d": "🌥",
-        "05d": "🚿",
-        "06d": "🌧",
-        "07d": "🌩",
-        "08d": "⛄",
-        "09d": "🕶"
-      }
-    };
   }
 };
 </script>
